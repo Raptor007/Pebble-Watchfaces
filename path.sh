4e97ec56206f97b6c40324e13db28e64aaf621f0
Raptor007 <blair@datapro.net>
2015-03-10 02:25:55 -0700

    SVN rev 78: Moved Python 2.7 framework bin ahead of default path.

--- path.sh
+++ path.sh
@@ -11,6 +11,16 @@ fi
 # Get the path to this script.
 HERE="$(cd $(dirname $THIS) >& /dev/null && pwd)"
 
+# Add Python 2.7 framework bin to the start of the path, if it's not already in there somewhere.
+PYTHON_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin"
+if [ -d "$PYTHON_PATH" ]
+then
+	if [ -z "$(echo $PATH | grep $PYTHON_PATH)" ]
+	then
+		export PATH="$PYTHON_PATH:$PATH"
+	fi
+fi
+
 # If we didn't choose a specific PebbleSDK by environment variable, find the latest.
 if [ -z "$PEBBLE_SDK" ]
 then
@@ -20,18 +30,8 @@ fi
 # Get the full path to the PebbleSDK/bin directory.
 BIN_PATH="$HERE/$PEBBLE_SDK/bin"
 
-# Add this PebbleSDK/bin directory to the start of the path, if it's not already there.
+# Add this PebbleSDK/bin directory to the start of the path, if it's not already in there somewhere.
 if [ -z "$(echo $PATH | grep $BIN_PATH)" ]
 then
 	export PATH="$BIN_PATH:$PATH"
-fi
-
-# For virtualenv (Pebble SDK 3) add Python framework bin to the end of the path.
-PYTHON_PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin"
-if [ -d "$PYTHON_PATH" ]
-then
-	if [ -z "$(echo $PATH | grep $PYTHON_PATH)" ]
-	then
-		export PATH="$PATH:$PYTHON_PATH"
-	fi
 fi
